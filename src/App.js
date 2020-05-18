614d0494da7fbca142cd22a6d9b46006d4965fd3
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-05-18 16:48:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+import LoginComponent from './components/LoginComponent'
+import SignUpComponent from './components/SignUpComponent'
+import {autoLogin} from './actions/userActions'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  componentDidMount(){
+    this.props.autoLogin() 
+  }
+
+  render(){
+    return (
+      <div className="App">
+            {
+              !this.props.userReducer.loggedIn ? <h1>Sign Up or Login!</h1> : <h1>Welcome, {this.props.userReducer.user.username}</h1>
+            }
+           <SignUpComponent/>
+           <LoginComponent/>
+           <button>Logout</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    userReducer: state.userReducer
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    autoLogin: () => dispatch(autoLogin())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import userReducer from './userReducer'; 
+import commentReducer from './commentReducer'; 
+import {combineReducers} from 'redux'; 
+
+
+const rootReducer = combineReducers({
+    userReducer,
+    commentReducer
+})
+
+export default rootReducer 
